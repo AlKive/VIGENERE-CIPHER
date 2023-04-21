dfc307088ebde8846f174cdfd9c8c60c138a68e9
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-21 18:39:52 +0800

    takes in alphabetic characters only from the users

--- VIGENERE-CIPHER.py
+++ VIGENERE-CIPHER.py
@@ -6,3 +6,17 @@ from termcolor import colored
 import pyfiglet
 from colorama import Back, Fore, Style, init
 import time
+
+# this function takes in alphabetic characters only from the users
+
+
+def _user_text(plaintext, key):
+    user_key = ''
+    i = 0
+    for char in plaintext:
+        if char.isalpha():
+            user_key += key[i % len(key)]
+            i += 1
+        else:
+            user_key += ' '
+    return user_key
